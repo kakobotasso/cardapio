9b700631bddeeae45a10dfd786978029aaa5385f
Kako Botasso <kakodev@gmail.com>
2012-10-23 20:30:01 -0200

    Preparando para enviar para outra tela[

--- src/br/com/kakobotasso/cardapio/Main.java
+++ src/br/com/kakobotasso/cardapio/Main.java
@@ -2,16 +2,22 @@ package br.com.kakobotasso.cardapio;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Main extends Activity {
 	private Button btEntrarHome;
+	private Intent it;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        associarElementos();
+        mudarTela();
     }
 
     @Override
@@ -22,6 +28,15 @@ public class Main extends Activity {
     
     public void associarElementos(){
     	this.btEntrarHome = (Button) findViewById(R.id.btEntrarHome);
+    	this.it = new Intent(this, Mesas.class);
+    }
+    
+    public void mudarTela(){
+    	this.btEntrarHome.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startActivity(it);
+			}
+		});
     }
 
 }
